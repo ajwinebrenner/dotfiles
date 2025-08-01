f458ab0d63bf0fb8879b29018d748a3f7f0fe36f
Andrew Winebrenner <andrewjwinebrenner@gmail.com>
2025-08-01 14:15:06 +0100

    add helpful keybinds

--- dot-config/zsh/behave.zsh
+++ dot-config/zsh/behave.zsh
@@ -8,7 +8,7 @@ HISTSIZE="50000"
 SAVEHIST="10000"
 setopt share_history
 setopt hist_expire_dups_first
-setopt hist_ignore_dups
+setopt hist_ignore_all_dups
 setopt hist_ignore_space
 setopt hist_verify
 
